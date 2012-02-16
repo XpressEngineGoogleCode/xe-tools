769d4ffefab8e323dd5f3385d370d7f8967df15c
ccata17@gmail.com <ccata17@gmail.com@3557e435-be14-93b3-dcab-2769928054c8>
2012-02-16 14:33:27 +0000

    XE Command Line Generator Tool - added template files

---
+++ trunk/generator/templates/template.admin.controller.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+     * @class  $PARAM_MODULE_NAMEAdminController
+     * @author $PARAM_MODULE_AUTHOR_NAME ($PARAM_MODULE_AUTHOR_EMAIL_ADDRESS)
+     * @brief  $PARAM_MODULE_NAME module of the admin controller class
+     **/
+	class $PARAM_MODULE_NAMEAdminController extends $PARAM_MODULE_NAME {
+		/**
+         * @brief Initialization
+         **/
+        function init() {
+        }
+	}
+?>
---
+++ trunk/generator/templates/template.admin.view.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * @class  $PARAM_MODULE_NAMEAdminView
+     * @author $PARAM_MODULE_AUTHOR_NAME ($PARAM_MODULE_AUTHOR_EMAIL_ADDRESS)
+     * @brief  $PARAM_MODULE_NAME admin view of the module class
+     **/
+    class $PARAM_MODULE_NAMEAdminView extends $PARAM_MODULE_NAME {
+        /**
+         * @brief Initialization
+         **/
+        function init() {
+		}		
+	}
+?>
