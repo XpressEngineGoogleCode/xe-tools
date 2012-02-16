fae48125b61cc8c8589ca26c267766720a5c92b8
ccata17 <ccata17@gmail.com>
2012-02-16 14:33:27 +0000

    XE Command Line Generator Tool - added template files

---
+++ generator/templates/template.controller.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * @class  $PARAM_MODULE_NAMEController
+	 * @author $PARAM_MODULE_AUTHOR_NAME ($PARAM_MODULE_AUTHOR_EMAIL_ADDRESS)
+	 * @brief  Controller for the $PARAM_MODULE_NAME module
+	 **/
+	class $PARAM_MODULE_NAMEController extends $PARAM_MODULE_NAME {
+		/**
+		 * @brief Initialization
+		 **/
+		function init() {
+		}
+	}
+?>
