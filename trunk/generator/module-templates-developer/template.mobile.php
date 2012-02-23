d56a76cf1f9ef58305e02264a7b0bd7da91f484f
ccata17@gmail.com <ccata17@gmail.com@3557e435-be14-93b3-dcab-2769928054c8>
2012-02-23 21:07:01 +0000

    XE Command Line Generator Tool - update (developer module generation)

---
+++ trunk/generator/module-templates-developer/template.mobile.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+     * @class  $PARAM_MODULE_NAMEMobile
+     * @author $PARAM_MODULE_AUTHOR_NAME ($PARAM_MODULE_AUTHOR_EMAIL_ADDRESS)
+     * @brief  $PARAM_MODULE_NAME mobile of the module class
+     **/
+	class $PARAM_MODULE_NAMEMobile extends $PARAM_MODULE_NAME{
+		/**
+         * @brief Initialization
+         **/
+		function init() {
+		}
+	}
+?>
---
+++ trunk/generator/module-templates-developer/template.view.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * @class  $PARAM_MODULE_NAMEView
+     * @author $PARAM_MODULE_AUTHOR_NAME ($PARAM_MODULE_AUTHOR_EMAIL_ADDRESS)
+     * @brief  View class of the $PARAM_MODULE_NAME module
+     **/
+    class $PARAM_MODULE_NAMEView extends $PARAM_MODULE_NAME {
+        /**
+         * @brief Initialization
+         **/
+        function init() {
+        }
+    }
+?>
