28ce4ea793cb968febf0b36b7fbac5a79ee5b7ca
ccata17@gmail.com <ccata17@gmail.com@3557e435-be14-93b3-dcab-2769928054c8>
2012-02-27 16:56:36 +0000

    XE Command Line Generator Tool - update (basic addon generation)

---
+++ trunk/generator/addon-templates-basic/template.addon.php
@@ -0,0 +1,26 @@
+<?php
+if(!defined('__XE__')) exit();
+
+/**
+ * @file	$PARAM_ADDON_NAME.addon.php
+ * @author	$PARAM_ADDON_AUTHOR_NAME ($PARAM_ADDON_AUTHOR_EMAIL_ADDRESS)
+ * @brief	$PARAM_ADDON_DESCRIPTION
+ **/
+ 
+// switch by entry point
+ switch($called_position)
+ {
+	// Before creating a module object:  After finding a necessary module upon a user request and before creating the object of that module.
+	case 'before_module_init':
+		break;
+	// Before executing a module:  After executing the object of a module and before executing the module.
+	case 'before_module_proc':
+		break;
+	// After executing a module:  Immediately after executing a created module object and obtaining the result.
+	case 'after_module_proc':
+		break;
+	// Before displaying result: Immediately before displaying the result of a module to which a layout has been applied.
+	case 'before_display_content':
+		break;
+ }
+?>
