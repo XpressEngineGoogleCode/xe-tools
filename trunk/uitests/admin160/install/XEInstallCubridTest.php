33631a1a871f0efa3ffedd5d1d7d446090ed102d
corina.udrescu@gmail.com <corina.udrescu@gmail.com@3557e435-be14-93b3-dcab-2769928054c8>
2012-04-23 17:30:34 +0000

    Added UI tests for XE 1.6 installation with CUBRID

---
+++ trunk/uitests/admin160/install/XEInstallCubridTest.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'XEInstallTest.php';
+
+class XEInstallCubridTest extends XEInstallTest
+{
+	protected static $oXESelenium;
+	protected static $errorScreenFile = './install.cubrid.error.png';
+	protected static $property = './install.cubrid.properties';
+}
