32ff21b426af6ca4c386a17363b996937fb0c52a
corina.udrescu <corina.udrescu@gmail.com>
2012-04-25 10:50:27 +0000

    One more update for path - admin installation 1.6

--- uitests/admin160/install/XEInstallCubridTest.php
+++ uitests/admin160/install/XEInstallCubridTest.php
@@ -4,6 +4,6 @@ include_once 'XEInstallTest.php';
 class XEInstallCubridTest extends XEInstallTest
 {
 	protected static $oXESelenium;
-	protected static $errorScreenFile = './install.cubrid.error.png';
-	protected static $property = './install.cubrid.properties';
+	protected static $errorScreenFile = 'install.cubrid.error.png';
+	protected static $property = 'install.cubrid.properties';
 }
--- uitests/admin160/install/XEInstallMySQLTest.php
+++ uitests/admin160/install/XEInstallMySQLTest.php
@@ -3,7 +3,7 @@ include_once 'XEInstallTest.php';
 
 class XEInstallMySQLTest extends XEInstallTest
 {
-	protected static $errorScreenFile = './install.mysql.error.png';
-	protected static $property = './install.mysql.properties';
+	protected static $errorScreenFile = 'install.mysql.error.png';
+	protected static $property = 'install.mysql.properties';
 
 }
