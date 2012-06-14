c9ce970ee306984349518dc41a5a4ec162f9904d
ovidiu.veliscu <ovidiu.veliscu@3557e435-be14-93b3-dcab-2769928054c8>
2012-06-14 14:42:22 +0000

    Instant XE - the GUI calls the methods from the console version using Dispatcher class

--- trunk/instant-xe/src/setup/Command.java
+++ trunk/instant-xe/src/setup/Command.java
@@ -1,16 +1,18 @@
 package setup;
 
+import java.util.ArrayList;
+
 /**
  * Holds the command to be executed and the command type
  */
 public class Command {
-	String cmd;					//command to execute
+	ArrayList<String> cmd;		//command to execute
 	CommandType commandType;	//command type (CREATE_INSTANCE, PAUSE_INSTANCE, ASSOCIATE_DOMAIN etc...)
 
 	/**
 	 * Retains the given command to execute and its type
 	 */
-	public Command(String cmd, CommandType commandType) {
+	public Command(ArrayList<String> cmd, CommandType commandType) {
 		this.cmd = cmd;
 		this.commandType = commandType;
 	}
