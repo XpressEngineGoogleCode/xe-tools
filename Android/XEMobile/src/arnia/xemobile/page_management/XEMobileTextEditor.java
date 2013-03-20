631c049062cd29de72f30becd7553f2c79125263
leap.bun <leap.bun@fsi.com.kh>
2013-03-20 11:06:11 +0000

    Add link button to text editor

--- Android/XEMobile/src/arnia/xemobile/page_management/XEMobileTextEditor.java
+++ Android/XEMobile/src/arnia/xemobile/page_management/XEMobileTextEditor.java
@@ -1,11 +1,15 @@
 package arnia.xemobile.page_management;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.Selection;
 import android.text.Spannable;
 import android.text.TextWatcher;
 import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
 import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,9 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView.BufferType;
 import android.widget.ToggleButton;
 import arnia.xemobile.R;
 import arnia.xemobile.XEFragment;
@@ -30,6 +37,9 @@ public class XEMobileTextEditor extends XEFragment implements OnClickListener,
 
 	private int styleStart = -1;
 	private int cursorLoc = 0;
+
+	private int tmpSelectionStart;
+	private int tmpSelectionEnd;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,6 +67,17 @@ public class XEMobileTextEditor extends XEFragment implements OnClickListener,
 		btnAddImage.setOnClickListener(this);
 
 		return view;
+	}
+
+	// Refresh current cursor position variable value
+	private void refreshSelectionPosition() {
+		if (txtContent.getSelectionStart() > txtContent.getSelectionEnd()) {
+			tmpSelectionStart = txtContent.getSelectionEnd();
+			tmpSelectionEnd = txtContent.getSelectionStart();
+		} else {
+			tmpSelectionStart = txtContent.getSelectionStart();
+			tmpSelectionEnd = txtContent.getSelectionEnd();
+		}
 	}
 
 	@Override
@@ -145,6 +166,9 @@ public class XEMobileTextEditor extends XEFragment implements OnClickListener,
 			break;
 		case R.id.XEMOBILE_EDITOR_UNDERLINE:
 			underlineClick();
+			break;
+		case R.id.XEMOBILE_EDITOR_LINK:
+			linkClick();
 			break;
 		}
 	}
@@ -254,4 +278,68 @@ public class XEMobileTextEditor extends XEFragment implements OnClickListener,
 		}
 	}
 
+	private void linkClick() {
+		refreshSelectionPosition();
+
+		AlertDialog.Builder linkBox = new AlertDialog.Builder(getActivity());
+
+		linkBox.setTitle(getString(R.string.link));
+
+		final LinearLayout layout = new LinearLayout(getActivity());
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.setPadding(10, 0, 10, 0);
+
+		final EditText txtLinkText = new EditText(getActivity());
+		txtLinkText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT));
+		txtLinkText.setHint(getString(R.string.link_text));
+
+		final EditText txtLinkUrl = new EditText(getActivity());
+		txtLinkUrl.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT));
+		txtLinkUrl.setHint(getString(R.string.link_url));
+
+		if (tmpSelectionStart < tmpSelectionEnd) {
+			txtLinkText.setText(txtContent.getText().subSequence(
+					tmpSelectionStart, tmpSelectionEnd));
+			txtLinkUrl.requestFocus();
+		}
+		
+		layout.addView(txtLinkText);
+		layout.addView(txtLinkUrl);
+
+		linkBox.setView(layout);
+		linkBox.setPositiveButton(R.string.ok,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						txtContent.getText().replace(tmpSelectionStart,
+								tmpSelectionEnd, txtLinkText.getText());
+						tmpSelectionEnd = tmpSelectionStart
+								+ txtLinkText.getText().length();
+						String linkUrl = txtLinkUrl.getText().toString();
+						Spannable str = txtContent.getText();
+						str.setSpan(new URLSpan(linkUrl), tmpSelectionStart,
+								tmpSelectionEnd,
+								Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+					}
+				});
+		linkBox.setNegativeButton(R.string.cancel, null);
+		linkBox.show();
+	}
+
+	public String getContent() {
+		return Html.toHtml(txtContent.getText());
+	}
+
+	public void setContent(String htmlContent) {
+		txtContent.setText(Html.fromHtml(htmlContent), BufferType.SPANNABLE);
+	}
+
+	public void requestFocus(){
+		txtContent.requestFocus();
+	}
+	
 }
