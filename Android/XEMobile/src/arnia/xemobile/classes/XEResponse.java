438d98d085e7288139db087c625f9a11d267ce42
leap.bun <leap.bun@fsi.com.kh>
2013-03-20 11:07:19 +0000

    Remove unused import

--- Android/XEMobile/src/arnia/xemobile/classes/XEResponse.java
+++ Android/XEMobile/src/arnia/xemobile/classes/XEResponse.java
@@ -1,18 +1,19 @@
 package arnia.xemobile.classes;
 
-import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
-@Root (name="response")
-public class XEResponse 
-{
-	@Element (required = false) 
+@Root(name = "response")
+public class XEResponse {
+	@Element(required = false)
 	public String value;
+
+	@Element(required = false)
+	public int error;
 	
-	@Element (required = false)
+	@Element(required = false)
 	public String message;
-	
-	@Element (required = false)
+
+	@Element(required = false)
 	public String document_srl;
 }
