411bcdb7e63f3718b8e1251f83054609fc2ee49f
leap.bun@fsi.com.kh <leap.bun@fsi.com.kh@3557e435-be14-93b3-dcab-2769928054c8>
2013-03-22 09:07:21 +0000

    Add pagination to posts

--- trunk/Android/XEMobile/src/arnia/xemobile/classes/XEArrayList.java
+++ trunk/Android/XEMobile/src/arnia/xemobile/classes/XEArrayList.java
@@ -3,46 +3,47 @@ package arnia.xemobile.classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 
+public class XEArrayList {
+	@Element(required = false)
+	public XEPagination pagination;
 
-
-public class XEArrayList
-{
-	@ElementList(inline=true,required=false)
+	@ElementList(inline = true, required = false)
 	public List<XEMember> members;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XEMenu> menus;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XEModule> modules;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XEPage> pages;
 
-	@ElementList(inline=true,required=false)
+	@ElementList(inline = true, required = false)
 	public ArrayList<XELayout> layouts;
 
-	@ElementList(inline=true,required=false)
+	@ElementList(inline = true, required = false)
 	public ArrayList<XEDayStats> stats;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XETextyle> textyles;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XETextylePost> posts;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XETextylePage> textylePages;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XESkin> skins;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XEComment> comments;
-	
-	@ElementList(inline=true,required=false)
+
+	@ElementList(inline = true, required = false)
 	public ArrayList<XETheme> themes;
-	
+
 }
